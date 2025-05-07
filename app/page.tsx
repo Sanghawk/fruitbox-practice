70dae17074efc1777590f7281cf53b6d3b4a3c9e
Sanghawk Lee <sanghawk@gmail.com>
2025-05-06 20:13:56 -0700

    removes setselect with rect null logic, changes names of box to fruitbox, fixes onpointerdown right click from drawing box

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 const DIM_ROW = 10;
 const DIM_COL = 10;
@@ -9,62 +9,88 @@ type Point = {
   y: number;
 };
 
+type Rect = {
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+};
+
 export default function Home() {
   return (
     <div>
-      <Box col={DIM_COL} row={DIM_ROW} />
+      <FruitBox col={DIM_COL} row={DIM_ROW} />
     </div>
   );
 }
 
-function Box({ col, row }: { col: number; row: number }) {
-  const boxRef = useRef<HTMLDivElement>(null);
-  const [isSelecting, setIsSelecting] = useState(false);
+function FruitBox({ col, row }: { col: number; row: number }) {
+  const containerRef = useRef<HTMLDivElement>(null);
   const [origin, setOrigin] = useState<Point | null>(null);
-  const [current, setCurrent] = useState<Point | null>(null);
+  const [rect, setRect] = useState<Rect | null>(null);
 
   function handlePointerLeave() {
-    setIsSelecting(false);
-    setOrigin({ x: 0, y: 0 });
-    setCurrent({ x: 0, y: 0 });
-    console.log("pointer leave");
+    setOrigin(null);
+    setRect(null);
   }
-  function handlePointerMove(e: React.PointerEvent<HTMLDivElement>) {
-    if (!boxRef.current) return;
-    const rect = boxRef.current?.getBoundingClientRect();
-    setCurrent({ x: e.clientX - rect.left, y: e.clientY - rect.top });
-    console.log("pointer move");
-  }
+
   function handlePointerDown(e: React.PointerEvent<HTMLDivElement>) {
-    if (!boxRef.current) return;
-    const rect = boxRef.current?.getBoundingClientRect();
-    console.log(rect, rect.left, rect.top);
-    setOrigin({ x: e.clientX - rect.left, y: e.clientY - rect.top });
-    setIsSelecting(true);
-    console.log("pointer down", "x", e.clientX, "y", e.clientY);
+    // If not left button or no container, do nothing
+    if (e.button !== 0 || !containerRef.current) return;
+    const containerBoundingRect = containerRef.current.getBoundingClientRect();
+
+    const originX = e.clientX - containerBoundingRect.left;
+    const originY = e.clientY - containerBoundingRect.top;
+    // Set the origin and rect
+    setOrigin({
+      x: originX,
+      y: originY,
+    });
+    setRect({
+      left: originX,
+      top: originY,
+      width: 0,
+      height: 0,
+    });
+  }
+
+  function handlePointerMove(e: React.PointerEvent<HTMLDivElement>) {
+    // If not selecting or no origin or no container, do nothing
+    if (!origin || !containerRef.current) return;
+    const containerBoundingRect = containerRef.current.getBoundingClientRect();
+
+    const originX = e.clientX - containerBoundingRect.left;
+    const originY = e.clientY - containerBoundingRect.top;
+    // Update the rect
+    setRect({
+      left: Math.min(origin.x, originX),
+      top: Math.min(origin.y, originY),
+      width: Math.abs(originX - origin.x),
+      height: Math.abs(originY - origin.y),
+    });
   }
   function handlePointerUp() {
-    setIsSelecting(false);
-    console.log("pointer up");
+    setOrigin(null);
+    setRect(null);
   }
   return (
     <div className="h-screen flex items-center justify-center">
       <div
-        ref={boxRef}
+        ref={containerRef}
         onPointerLeave={handlePointerLeave}
         onPointerMove={handlePointerMove}
         onPointerDown={handlePointerDown}
         onPointerUp={handlePointerUp}
         className="relative w-[500px] h-[500px] border-1"
       >
-        {isSelecting && (
+        {rect && (
           <div
             className="absolute border-1"
             style={{
-              left: Math.min(origin.x, current.x),
-              top: Math.min(origin.y, current.y),
-              width: Math.abs(current.x - origin.x),
-              height: Math.abs(current.y - origin.y),
+              left: rect.left,
+              top: rect.top,
+              width: rect.width,
+              height: rect.height,
             }}
           ></div>
         )}
