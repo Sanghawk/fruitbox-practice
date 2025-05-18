c9ac6ab813436937eeb84348d14c8cd9cf7e5d26
Sanghawk Lee <sanghawk@gmail.com>
2025-05-17 19:32:38 -0700

    made activescreen partially more responsive

--- app/tenzy/views/ActiveScreen/ActiveScreen.tsx
+++ app/tenzy/views/ActiveScreen/ActiveScreen.tsx
@@ -7,22 +7,18 @@ import GameGrid from "@/app/tenzy/components/GameGrid";
 export default function ActiveScreen() {
   return (
     <CtxConditionalRenderActiveScreen>
-      <div className="sticky top-[64px] h-[calc(100dvh-64px)] ">
+      <div className="min-h-full flex flex-col justify-center items-center py-4 gap-4">
         <div
-          className="mx-auto h-full"
-          style={{ width: `${BOARD_COLS * CELL_SIZE}px` }}
+          className=" flex flex-row w-full justify-between items-end"
+          style={{ maxWidth: `${BOARD_COLS * CELL_SIZE}px` }}
         >
-          <div className="h-full flex flex-col justify-around gap-4">
-            <div className="flex flex-col justify-center gap-4 flex-1">
-              <div className="flex flex-row justify-between items-end">
-                <GameScore />
-                <ResetButton />
-              </div>
-              <div>
-                <TimeLeft />
-                <GameGrid />
-              </div>
-            </div>
+          <GameScore />
+          <ResetButton />
+        </div>
+        <div style={{ width: `${BOARD_COLS * CELL_SIZE}px` }}>
+          <div className="flex flex-col">
+            <TimeLeft />
+            <GameGrid />
           </div>
         </div>
       </div>
